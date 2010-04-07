4085863fb4cfa7477d18045b9ce3676c147c6bba
Alex Ott <alexott@gmail.com>
2010-04-07 17:09:52 +0200

    explicitly specify apacher release repository

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Interface to Apache Tika"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [org.apache.tika/tika-parsers "0.6"]
+                 [org.apache.tika/tika-parsers "0.7"]
                  ]
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
+  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]]
+  :repositories {"apache-releases" "https://repository.apache.org/content/repositories/releases/"}
+  )
