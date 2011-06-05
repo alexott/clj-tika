20a60c80352b4763f163519567deddcdb4113d5b
Alex Ott <alexott@gmail.com>
2011-06-05 20:20:55 +0200

    update deps

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-tika "1.0.0-SNAPSHOT"
-  :description "Interface to Apache Tika"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.apache.tika/tika-parsers "0.7"]
+  :description "Clojure bindings to Apache Tika"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.apache.tika/tika-parsers "0.9"]
                  ]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.3.1"]]
 ;  :repositories {"apache-releases" "http://repository.apache.org/content/repositories/releases/"}
   )
