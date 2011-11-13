d37e5252c103b9fbad0593c2572cced1d19e13a8
Alex Ott <alexott@gmail.com>
2011-11-13 19:53:23 +0100

    update to tika 1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
-(defproject clj-tika "1.0.0-SNAPSHOT"
+(defproject clj-tika "1.0.0"
   :description "Clojure bindings to Apache Tika"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.apache.tika/tika-parsers "0.9"]
-                 ]
-  :dev-dependencies [[swank-clojure "1.3.1"]]
-;  :repositories {"apache-releases" "http://repository.apache.org/content/repositories/releases/"}
+                 [org.apache.tika/tika-parsers "1.0"]]
   )
