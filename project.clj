6f46c562e5b676eb18bd396e7c64564c1bc36ab4
Alex Ott <alexott@gmail.com>
2012-11-20 17:01:46 +0100

    update to Tika 1.2

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-tika "1.1.1"
+(defproject clj-tika "1.2.0"
   :description "Clojure bindings to Apache Tika"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.tika/tika-parsers "1.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.apache.tika/tika-parsers "1.2"]]
   )
