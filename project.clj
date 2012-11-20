2609b78dc244caae063d2a63b6401e31b3364fb7
Florian Over <florian.over@googlemail.com>
2012-11-20 11:40:34 +0100

    Updated tika version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-tika "1.1.1"
   :description "Clojure bindings to Apache Tika"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.tika/tika-parsers "1.1"]]
+                 [org.apache.tika/tika-parsers "1.2"]]
   )
