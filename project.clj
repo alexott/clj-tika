628662a9989ff98bbd88976c79f915913acba860
Florian Over <florian.over@googlemail.com>
2013-01-23 09:43:16 +0100

    Upgraded tika version

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
-(defproject clj-tika "1.2.0"
+(defproject org.clojars.floriano.clj-tika "1.2.3"
   :description "Clojure bindings to Apache Tika"
+  :url "https://github.com/FlorianO/clj-tika"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.apache.tika/tika-parsers "1.2"]]
+                 [org.apache.tika/tika-parsers "1.3"]]
   )
